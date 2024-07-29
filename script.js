88c8f3ff58f7faeef844398b3cefe01ec63e5e19
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2024-07-29 08:35:12 +0530

    Obfuscated the js

--- script.js
+++ script.js
@@ -1,88 +1,155 @@
-q1 = "hello";
-qNum = 0;
-totalQ = 10;
-score = 0;
-answer = Array(totalQ-1);
-corrAns = Array (totalQ-1);
-
-
-function nextQ(){
-
-    //declaring objects
-    const btn = document.getElementById('nextButton');
-    const quesTxt = document.getElementById('question');
-    const ans = document.getElementById("answer");
-    const qnaScr = document.getElementById('qnaScr');
-    const endScr = document.getElementById('endScr');
-    const scoreTxt = document.getElementById('score');
-    const img = document.getElementById('qImg');
-
-    //creating an array for storing questions and correct answers
-    question = [
-        'question 1 placeholder',
-        '$$$',
-        '',
-        '49 54 56 53 13 10  u0076\\u0065\\u0067\\u0065\\u0074 \\u0061\\u0072\\u0069\\u0061\\u006e\\u000d -- ELLA',
-        'The dog is burning. The potrait was scratched. Long before you reached the Bottom.',
-        'Sub-species of bird that buries its head into the sand, an element you have in hand. \n Atomically pronic, the colour bland, \n the number is important, keep that in your mind. \n Go back a century and tell me what you find........ ',
-        'Space won\x27t save you',
-        '£1 \n I can\x27t see and I can\x27t hear, but I can write with no fear.',
-        '34.0140281880751,-118.28751964143076 \n What do people call me? I am very fast. \n Where do I migrate in hot weather forecast?',
-        'Flying'
-    ];
-
-    image = [
-        'css/Q1.jpg',
-        'css/Q2.jpg',
-        'css/Q3.png',
-        'css/Q4.png',
-        'css/Q5.png',
-        '',
-        'css/Q7.png',
-        'css/Q8.png',
-        '',
-        'css/Q10.png'
-    ];
-
-    corrAns = [
-        'George Stafford',
-        '3.47',
-        'Phacidiaceae',
-        'Livonian War',
-        'Sirius Black',
-        'Fat Man',
-        'Mackenzie McDonald',
-        'Helen Brawn',
-        'Canada',
-        'Mercedes CLR'
-    ];
-    
-
-    //SAVING ANSWER ----
-    answer[qNum] = ans.value;
-
-    ans.style.outlineColor = 'red';
-    //calculating score
-    if (answer[qNum] == corrAns[qNum]){
-        
-        //RESETTING VALUE OF TEXTBOX AND COLOR---- 
-        ans.value = "";
-        ans.style.outlineColor = "#222";
-
-        //ADDING TO TOTAL NO. OF QUESTIONS AND SCORE----
-        qNum++;
-        score++;
-
-        //CHANGING QUESTION TEXT AND IMAGE------
-        img.src = image[qNum];
-        quesTxt.innerText = (qNum+1) + "." + " " + question[qNum];
-    } 
-        
-
-    //ENDING
-    if (qNum > totalQ - 1){
-        //making end screen visible
-        qnaScr.style.visibility = "hidden";
-        endScr.style.visibility = "visible";        
+function _0x5c01(_0x39b6f1, _0x6e05d4) {
+  const _0x5dd567 = _0x5dd5();
+  return (
+    (_0x5c01 = function (_0x5c011c, _0x5da4a7) {
+      _0x5c011c = _0x5c011c - 0x1e2;
+      let _0xce0704 = _0x5dd567[_0x5c011c];
+      return _0xce0704;
+    }),
+    _0x5c01(_0x39b6f1, _0x6e05d4)
+  );
+}
+(function (_0x4c7beb, _0x6aa420) {
+  const _0x471a91 = _0x5c01,
+    _0x121c83 = _0x4c7beb();
+  while (!![]) {
+    try {
+      const _0x2ea114 =
+        (-parseInt(_0x471a91(0x1ea)) / 0x1) *
+          (parseInt(_0x471a91(0x1fb)) / 0x2) +
+        (-parseInt(_0x471a91(0x205)) / 0x3) *
+          (-parseInt(_0x471a91(0x1f0)) / 0x4) +
+        (-parseInt(_0x471a91(0x20d)) / 0x5) *
+          (-parseInt(_0x471a91(0x1ee)) / 0x6) +
+        (-parseInt(_0x471a91(0x208)) / 0x7) *
+          (parseInt(_0x471a91(0x1f8)) / 0x8) +
+        -parseInt(_0x471a91(0x1fe)) / 0x9 +
+        (parseInt(_0x471a91(0x200)) / 0xa) *
+          (parseInt(_0x471a91(0x207)) / 0xb) +
+        parseInt(_0x471a91(0x1f2)) / 0xc;
+      if (_0x2ea114 === _0x6aa420) break;
+      else _0x121c83["push"](_0x121c83["shift"]());
+    } catch (_0x148f6c) {
+      _0x121c83["push"](_0x121c83["shift"]());
     }
+  }
+})(_0x5dd5, 0x92487),
+  (q1 = "hello"),
+  (qNum = 0x0),
+  (totalQ = 0xa),
+  (score = 0x0),
+  (answer = Array(totalQ - 0x1)),
+  (corrAns = Array(totalQ - 0x1));
+function _0x5dd5() {
+  const _0x475ad0 = [
+    "21MMXjrq",
+    "Livonian\x20War",
+    "css/Q5.png",
+    "Helen\x20Brawn",
+    "visibility",
+    "235285hXxVYg",
+    "css/Q3.png",
+    "visible",
+    "Space\x20won\x27t\x20save\x20you",
+    "answer",
+    "Phacidiaceae",
+    "css/Q8.png",
+    "red",
+    "css/Q1.jpg",
+    "$$$",
+    "value",
+    "4439giifVD",
+    "outlineColor",
+    "Sub-species\x20of\x20bird\x20that\x20buries\x20its\x20head\x20into\x20the\x20sand,\x20an\x20element\x20you\x20have\x20in\x20hand.\x20\x0a\x20Atomically\x20pronic,\x20the\x20colour\x20bland,\x20\x0a\x20the\x20number\x20is\x20important,\x20keep\x20that\x20in\x20your\x20mind.\x20\x0a\x20Go\x20back\x20a\x20century\x20and\x20tell\x20me\x20what\x20you\x20find........\x20",
+    "Flying",
+    "6zvXKoH",
+    "#222",
+    "452204PvkIuw",
+    "£1\x20\x0a\x20I\x20can\x27t\x20see\x20and\x20I\x20can\x27t\x20hear,\x20but\x20I\x20can\x20write\x20with\x20no\x20fear.",
+    "3174528QuZgMe",
+    "Mackenzie\x20McDonald",
+    "49\x2054\x2056\x2053\x2013\x2010\x20\x20u0076\x5cu0065\x5cu0067\x5cu0065\x5cu0074\x20\x5cu0061\x5cu0072\x5cu0069\x5cu0061\x5cu006e\x5cu000d\x20--\x20ELLA",
+    "qnaScr",
+    "css/Q2.jpg",
+    "score",
+    "1436656qRucME",
+    "css/Q4.png",
+    "innerText",
+    "334FluDld",
+    "style",
+    "hidden",
+    "5110569dgQnqe",
+    "qImg",
+    "360650PZkrdn",
+    "nextButton",
+    "The\x20dog\x20is\x20burning.\x20The\x20potrait\x20was\x20scratched.\x20Long\x20before\x20you\x20reached\x20the\x20Bottom.",
+    "css/Q10.png",
+    "Canada",
+    "27daDKkO",
+    "getElementById",
+    "341edBfeO",
+  ];
+  _0x5dd5 = function () {
+    return _0x475ad0;
+  };
+  return _0x5dd5();
+}
+function nextQ() {
+  const _0x3cc42d = _0x5c01,
+    _0x2993e5 = document["getElementById"](_0x3cc42d(0x201)),
+    _0x3da3a9 = document[_0x3cc42d(0x206)]("question"),
+    _0x3408bc = document[_0x3cc42d(0x206)](_0x3cc42d(0x1e3)),
+    _0x209f12 = document[_0x3cc42d(0x206)](_0x3cc42d(0x1f5)),
+    _0x3ebafe = document[_0x3cc42d(0x206)]("endScr"),
+    _0x3cf721 = document[_0x3cc42d(0x206)](_0x3cc42d(0x1f7)),
+    _0x10901c = document[_0x3cc42d(0x206)](_0x3cc42d(0x1ff));
+  (question = [
+    "question\x201\x20placeholder",
+    _0x3cc42d(0x1e8),
+    "",
+    _0x3cc42d(0x1f4),
+    _0x3cc42d(0x202),
+    _0x3cc42d(0x1ec),
+    _0x3cc42d(0x1e2),
+    _0x3cc42d(0x1f1),
+    "34.0140281880751,-118.28751964143076\x20\x0a\x20What\x20do\x20people\x20call\x20me?\x20I\x20am\x20very\x20fast.\x20\x0a\x20Where\x20do\x20I\x20migrate\x20in\x20hot\x20weather\x20forecast?",
+    _0x3cc42d(0x1ed),
+  ]),
+    (image = [
+      _0x3cc42d(0x1e7),
+      _0x3cc42d(0x1f6),
+      _0x3cc42d(0x20e),
+      _0x3cc42d(0x1f9),
+      _0x3cc42d(0x20a),
+      "",
+      "css/Q7.png",
+      _0x3cc42d(0x1e5),
+      "",
+      _0x3cc42d(0x203),
+    ]),
+    (corrAns = [
+      "George\x20Stafford",
+      "3.47",
+      _0x3cc42d(0x1e4),
+      _0x3cc42d(0x209),
+      "Sirius\x20Black",
+      "Fat\x20Man",
+      _0x3cc42d(0x1f3),
+      _0x3cc42d(0x20b),
+      _0x3cc42d(0x204),
+      "Mercedes\x20CLR",
+    ]),
+    (answer[qNum] = _0x3408bc[_0x3cc42d(0x1e9)]),
+    (_0x3408bc[_0x3cc42d(0x1fc)][_0x3cc42d(0x1eb)] = _0x3cc42d(0x1e6)),
+    answer[qNum] == corrAns[qNum] &&
+      ((_0x3408bc[_0x3cc42d(0x1e9)] = ""),
+      (_0x3408bc["style"][_0x3cc42d(0x1eb)] = _0x3cc42d(0x1ef)),
+      qNum++,
+      score++,
+      (_0x10901c["src"] = image[qNum]),
+      (_0x3da3a9[_0x3cc42d(0x1fa)] =
+        qNum + 0x1 + "." + "\x20" + question[qNum])),
+    qNum > totalQ - 0x1 &&
+      ((_0x209f12[_0x3cc42d(0x1fc)][_0x3cc42d(0x20c)] = _0x3cc42d(0x1fd)),
+      (_0x3ebafe["style"][_0x3cc42d(0x20c)] = _0x3cc42d(0x20f)));
 }
